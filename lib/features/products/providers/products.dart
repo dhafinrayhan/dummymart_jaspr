03db1fc5c65f3ea1e77ba25244493ae8fcb4ff45
Dhafin Rayhan <dhafindiamle@gmail.com>
2024-04-15 01:53:02 +0700

    Make provider auto dispose

--- lib/features/products/providers/products.dart
+++ lib/features/products/providers/products.dart
@@ -3,6 +3,6 @@ import 'package:jaspr_riverpod/jaspr_riverpod.dart';
 import '../../../services/api/api_client_provider.dart';
 import '../models/product.dart';
 
-final productsProvider = FutureProvider<List<Product>>(
+final productsProvider = FutureProvider.autoDispose<List<Product>>(
   (ref) => ref.watch(apiClientProvider).fetchProducts(),
 );
